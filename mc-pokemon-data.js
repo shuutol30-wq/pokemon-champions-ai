5845ba8d17376005fcd42ca57bab1e24e3cdac72
shuutol30-wq <shuutol30@gmail.com>
2026-09-17 22:06:25 +0900

    Create mc-pokemon-data.js

---
+++ mc-pokemon-data.js
@@ -0,0 +1,166 @@
+/*
+ * Pokémon Champions - Regulation M-C
+ * Newly featured Pokémon data
+ */
+
+const MC_POKEMON_DATA = [
+  {
+    name: "Wigglytuff",
+    types: ["Normal", "Fairy"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Persian",
+    types: ["Normal"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Persian-Alola",
+    displayName: "Persian (Alolan Form)",
+    types: ["Dark"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Farfetchd",
+    displayName: "Farfetch'd",
+    types: ["Normal", "Flying"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Mr-Mime",
+    displayName: "Mr. Mime",
+    types: ["Psychic", "Fairy"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Swalot",
+    types: ["Poison"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Salamence",
+    types: ["Dragon", "Flying"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Gogoat",
+    types: ["Grass"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Golisopod",
+    types: ["Bug", "Water"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Rillaboom",
+    types: ["Grass"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Cinderace",
+    types: ["Fire"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Inteleon",
+    types: ["Water"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Thievul",
+    types: ["Dark"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Toxtricity",
+    displayName: "Toxtricity (Amped Form)",
+    types: ["Electric", "Poison"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Toxtricity-Low-Key",
+    displayName: "Toxtricity (Low Key Form)",
+    types: ["Electric", "Poison"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Grapploct",
+    types: ["Fighting"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Perrserker",
+    types: ["Steel"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Sirfetchd",
+    displayName: "Sirfetch'd",
+    types: ["Fighting"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Pincurchin",
+    types: ["Electric"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Indeedee",
+    displayName: "Indeedee (Male)",
+    types: ["Psychic", "Normal"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Indeedee-Female",
+    displayName: "Indeedee (Female)",
+    types: ["Psychic", "Normal"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Pawmot",
+    types: ["Electric", "Fighting"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Arboliva",
+    types: ["Grass", "Normal"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Squawkabilly",
+    displayName: "Squawkabilly (Green Plumage)",
+    types: ["Normal", "Flying"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Squawkabilly-Blue",
+    displayName: "Squawkabilly (Blue Plumage)",
+    types: ["Normal", "Flying"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Squawkabilly-Yellow",
+    displayName: "Squawkabilly (Yellow Plumage)",
+    types: ["Normal", "Flying"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Squawkabilly-White",
+    displayName: "Squawkabilly (White Plumage)",
+    types: ["Normal", "Flying"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Mabosstiff",
+    types: ["Dark"],
+    regulations: ["M-C"]
+  },
+  {
+    name: "Baxcalibur",
+    types: ["Dragon", "Ice"],
+    regulations: ["M-C"]
+  }
+];
+
+window.MC_POKEMON_DATA = MC_POKEMON_DATA;
