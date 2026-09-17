1a6434490ae9b4b9175c27f032649e9ef95e0563
shuutol30-wq <shuutol30@gmail.com>
2026-09-17 21:48:39 +0900

    Create mc-roster-additions.js

---
+++ mc-roster-additions.js
@@ -0,0 +1,46 @@
+/*
+ * Pokémon Champions - Regulation M-C additions
+ * Roster metadata
+ */
+
+const MC_ROSTER_ADDITIONS = {
+  regulation: "M-C",
+
+  newPokemon: [
+    "Wigglytuff",
+    "Persian",
+    "Alolan Persian",
+    "Farfetch'd",
+    "Mr. Mime",
+    "Swalot",
+    "Salamence",
+    "Gogoat",
+    "Golisopod",
+    "Rillaboom",
+    "Cinderace",
+    "Inteleon",
+    "Thievul",
+    "Toxtricity",
+    "Grapploct",
+    "Perrserker",
+    "Sirfetch'd",
+    "Pincurchin",
+    "Indeedee",
+    "Pawmot",
+    "Arboliva",
+    "Squawkabilly",
+    "Mabosstiff",
+    "Baxcalibur"
+  ],
+
+  newMegaEvolutions: [
+    "Mega Salamence",
+    "Mega Golisopod",
+    "Mega Baxcalibur",
+    "Mega Absol Z",
+    "Mega Garchomp Z",
+    "Mega Lucario Z"
+  ]
+};
+
+window.MC_ROSTER_ADDITIONS = MC_ROSTER_ADDITIONS;
